3a0bf7e66a80c41f7280715ebb65b6d39e24ddb7
Jovidon <you@example.comjova_94@inbox.ru>
2019-09-04 16:03:29 -0700

    changes

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "html:target/default-cucumber-reports",
                 "rerun:target/rerun.txt"
         },
-       tags={"@demo_3"},              // ~ used to ignore the feature to run
+       tags={"@smoke_test"},              // ~ used to ignore the feature to run
         features = {   "src/test/resources/features"  }, // to specify where are the features
 
         //feature contains scenarios // every scenario is like test
